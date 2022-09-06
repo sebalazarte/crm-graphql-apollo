9ee6908c7ee3f7399575ddc08321f81d1b9ab470
Andres Sebastian Lazarte <sebalazarte@gmail.com>
2022-09-06 09:09:51 -0300

    esquema Usuario, primer mutation

--- db/resolvers.js
+++ db/resolvers.js
@@ -1,8 +1,35 @@
+const Usuario = require("../models/Usuario");
+const bcryptjs = require('bcryptjs');
 
 const resolvers = {
-    Query: {
-        holaMundo: () => "Hola mundo"
-    }
-}
+  Query: {
+    holaMundo: () => "Hola mundo",
+  },
+  Mutation: {
+    nuevoUsuario: async (_, { input }) => {
+      const { email, password } = input;
+
+      //revisar si el usuario ya esta registrado
+      const existeUsuario = await Usuario.findOne({ email });
+      if (existeUsuario) {
+        throw new Error("El usuario ya esta registrado");
+      }
+
+      //hasear su password
+      const salt = await bcryptjs.genSalt(10);
+      input.password = await bcryptjs.hash(password, salt);
+
+      try {
+          //guardar en la base de datos
+        const usuario = new Usuario(input);
+        usuario.save();
+        return usuario;
+
+      } catch (error) {
+        console.log(error);
+      }
+    },
+  },
+};
 
 module.exports = resolvers;
--- db/schema.js
+++ db/schema.js
@@ -2,9 +2,30 @@ const { gql } = require("apollo-server");
 
 //schema
 const typeDefs = gql`
+
+    type Usuario {
+        id: ID
+        nombre: String
+        apellido: String
+        email: String
+        creado: String
+    }
+
+    input UsuarioInput {
+        nombre: String!
+        apellido: String!
+        email: String!
+        password: String!
+    }
+
     type Query {
         holaMundo: String
     }
+
+    type Mutation {
+        nuevoUsuario(input: UsuarioInput) : Usuario
+    }
+
 `;
 
 module.exports = typeDefs;
