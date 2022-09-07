b3667fff984256c099f3ffcb56ba63f7f0cc96c1
Andres Sebastian Lazarte <sebalazarte@gmail.com>
2022-09-07 12:02:34 -0300

    JWT

--- db/resolvers.js
+++ db/resolvers.js
@@ -1,9 +1,28 @@
 const Usuario = require("../models/Usuario");
-const bcryptjs = require('bcryptjs');
+const bcryptjs = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+require("dotenv").config("../variables.env");
+
+const crearToken = (usuario, secreta, expiracion) => {
+  const { id, email, nombre, apellido } = usuario;
+  return jwt.sign(
+    {
+      id,
+      email,
+      nombre,
+      apellido,
+    },
+    secreta,
+    { expiresIn: expiracion }
+  );
+};
 
 const resolvers = {
   Query: {
-    holaMundo: () => "Hola mundo",
+    obtenerUsuarioAutenticado: (_, { token }) => {
+        const usuario = jwt.verify(token, process.env.SECRETA);
+        return usuario;
+      },
   },
   Mutation: {
     nuevoUsuario: async (_, { input }) => {
@@ -20,15 +39,41 @@ const resolvers = {
       input.password = await bcryptjs.hash(password, salt);
 
       try {
-          //guardar en la base de datos
+        //guardar en la base de datos
         const usuario = new Usuario(input);
         usuario.save();
         return usuario;
-
       } catch (error) {
         console.log(error);
       }
     },
+
+    autenticarUsuario: async (_, { input }) => {
+      const { email, password } = input;
+
+      //chequear si el usuario existe
+      const existeUsuario = await Usuario.findOne({ email });
+      if (!existeUsuario) {
+        throw new Error("El usuario no existe");
+      }
+
+      //revisar si el password es correcto
+      const passwordCorrecto = await bcryptjs.compare(
+        password,
+        existeUsuario.password
+      );
+
+      if (!passwordCorrecto) {
+        throw new Error("El password no es correcto");
+      }
+
+      //crear el token
+      return {
+        token: crearToken(existeUsuario, process.env.SECRETA, "24h"),
+      };
+    },
+
+    
   },
 };
 
